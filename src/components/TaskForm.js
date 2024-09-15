a54a8974d7cd9fb8fe88a28ee3c961a3cbd240b7
AlexTey99 <teyalex99@gmail.com>
2024-09-15 12:42:59 +0200

    My first commit

---
+++ src/components/TaskForm.js
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux";
+import { addTask, editTask } from "../features/tasks/taskSlice";
+import {v4 as uuid} from "uuid";
+import { useNavigate, useParams } from "react-router-dom";
+
+function TaskForm () {
+
+    const [task, setTask] = useState({
+        tittle: '',
+        description: ''
+    })
+
+    const dispatch = useDispatch();
+    const navegate = useNavigate();
+    const params = useParams();
+    const tasks = useSelector(state => state.tasks)
+
+    //Funcion para actualizar el estado  (Task) del componente Form
+    const handleChange = (e) => {
+        setTask({
+            ...task,
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); //Para evitar que se recargue la pagina
+
+        if(params.id){
+            dispatch(editTask(task))
+        }else {
+            dispatch(addTask({
+                ...task,
+                id: uuid()
+            }))
+        }
+        navegate('/')
+    }
+
+    useEffect(() => {
+        if(params.id) {
+            setTask(tasks.find((task) => task.id == params.id))
+        }
+    },[params.id, tasks])
+
+    return (
+        <form  onSubmit={handleSubmit} className="bg-zinc-800 max-w-sm p-4 mb-2">
+            <label htmlFor="tittle" className="block text-xs font-bold mb-2">Task:</label> 
+            <input name="tittle" value={task.tittle} type="text" className="w-full p-2 rounded-md bg-zinc-600 mb-2" placeholder="tittle" onChange={handleChange}/>
+            <label htmlFor="description" className="block text-xs font-bold mb-2">Description:</label> 
+            <textarea name="description" value={task.description} className="w-full p-2 rounded-md bg-zinc-600 mb-2" placeholder="description" onChange={handleChange}></textarea>
+            <button className="bg-indigo-600 px-2 py-1">Save</button>
+        </form>
+    )
+}
+
+export default TaskForm
---
+++ src/features/tasks/taskSlice.js
@@ -0,0 +1,47 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = [
+    {
+        id: "1",
+        tittle: "Task 1",
+        description: "Task 1 description",
+        completed: false
+    },
+    {
+        id: "2",
+        tittle: "Task 2",
+        description: "Task 2 description",
+        completed: false
+    }
+]
+
+export const taskSlice = createSlice({
+    name: 'tasks',
+    initialState: initialState,
+    reducers: {
+        addTask: (state, action) => {
+            state.push(action.payload)
+        },
+        editTask: (state, action) => {
+            const {id , tittle, description} = action.payload;
+
+            const foundTask = state.find(task => task.id == id)
+
+            if(foundTask) {
+                foundTask.tittle = tittle;
+                foundTask.description = description;
+            }
+        },
+        deleteTask: (state, action) => {
+           const taskFound = state.find(task => task.id == action.payload);
+           if(taskFound) {
+                state.splice(state.indexOf(taskFound), 1)
+           }
+        }
+
+    }
+})
+
+export const {addTask, deleteTask, editTask} = taskSlice.actions
+export default taskSlice.reducer;
